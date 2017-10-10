88941079ab8b4042f51526d48ec52cb175e10320
Gregory Pfeil <greg@technomadic.org>
2017-10-10 09:06:18 -0600

    Fix dyna/codyna.

--- unsafe/src/Yaya/Unsafe/Zoo.hs
+++ unsafe/src/Yaya/Unsafe/Zoo.hs
@@ -2,6 +2,7 @@ module Yaya.Unsafe.Zoo where
 
 import Control.Comonad.Cofree
 import Control.Monad.Trans.Free
+import Data.Functor.Identity
 
 import Yaya
 import Yaya.Control
@@ -11,11 +12,12 @@ import Yaya.Unsafe.Data
 chrono :: Functor f => GAlgebra (Cofree f) f b -> GCoalgebra (Free f) f a -> a -> b
 chrono = ghylo (distGHisto id) (distGFutu id)
 
--- codyna :: Functor f => Algebra f b -> GCoalgebra (Free f) f a -> a -> b
--- codyna = ghylo distCata $ distGFutu id
+codyna :: Functor f => Algebra f b -> GCoalgebra (Free f) f a -> a -> b
+codyna φ = ghylo distCata (distGFutu id) $ φ . fmap runIdentity
 
--- dyna :: Functor f => GAlgebra (Cofree f) f b -> Coalgebra f a -> a -> b
--- dyna = flip ghylo distAna $ distGHisto id
+-- | [Recursion Schemes for Dynamic Programming](https://www.researchgate.net/publication/221440162_Recursion_Schemes_for_Dynamic_Programming)
+dyna :: Functor f => GAlgebra (Cofree f) f b -> Coalgebra f a -> a -> b
+dyna φ ψ = ghylo (distGHisto id) distAna φ $ fmap Identity . ψ
 
 futu :: (Corecursive t f, Functor f) => GCoalgebra (Free f) f a -> a -> t
 futu = gana $ distGFutu id
