1b1dcae8326e57db4605aeee84787680acaa7b37
Greg Pfeil <greg@technomadic.org>
2024-02-26 06:49:41 -0700

    Add more instances for pattern functors
    Some already existed, but had been defined using `deriving-compat`, which
    requires @TemplateHaskell@ and thus breaks @Safe@. So this defines them
    explicitly.

--- core/src/Yaya/Pattern.hs
+++ core/src/Yaya/Pattern.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE TemplateHaskell #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 
 -- | Common pattern functors (and instances for them).
@@ -14,29 +13,80 @@ module Yaya.Pattern
   )
 where
 
-import Text.Show.Deriving (deriveShow1, deriveShow2)
 import "base" Control.Applicative (Applicative (..))
+import "base" Control.Category (Category ((.)))
 import "base" Control.Monad (Monad (..))
 import "base" Data.Bifunctor (Bifunctor (..))
-import "base" Data.Eq (Eq)
+import "base" Data.Bool (Bool (False, True), (&&))
+import "base" Data.Eq (Eq ((==)))
 import "base" Data.Foldable (Foldable)
 import "base" Data.Function (($))
 import "base" Data.Functor (Functor)
-import "base" Data.Ord (Ord)
+import "base" Data.Functor.Classes
+  ( Eq1 (liftEq),
+    Eq2 (liftEq2),
+    Ord1 (liftCompare),
+    Ord2 (liftCompare2),
+    Show1 (liftShowsPrec),
+    Show2 (liftShowsPrec2),
+  )
+import "base" Data.Ord (Ord (compare, (<=)), Ordering (EQ, GT, LT))
+import "base" Data.Semigroup ((<>))
 import "base" Data.Traversable (Traversable)
-import "base" Text.Show (Show)
+import qualified "base" Data.Tuple as Tuple
+import "base" GHC.Generics (Generic, Generic1)
+import "base" Text.Show (Show (showList, showsPrec), showParen, showString)
 import "comonad" Control.Comonad (Comonad (..))
 -- explicitly omitted import list for @strict@ modules
 import "strict" Data.Strict.Either
 import "strict" Data.Strict.Maybe
 import "strict" Data.Strict.Tuple
+import "base" Prelude (Num ((+)))
 
 -- | Isomorphic to 'Maybe (a, b)', it’s also the pattern functor for lists.
 data XNor a b = Neither | Both ~a b
-  deriving (Eq, Ord, Show, Functor, Foldable, Traversable)
+  deriving (Eq, Generic, Ord, Show, Foldable, Functor, Generic1, Traversable)
 
-deriveShow1 ''XNor
-deriveShow2 ''XNor
+instance (Eq a) => Eq1 (XNor a) where
+  -- TODO: Remove this once base-4.18 is the oldest supported verson, as it’s
+  --       the default impl.
+  liftEq = liftEq2 (==)
+
+instance Eq2 XNor where
+  liftEq2 f g = Tuple.curry $ \case
+    (Neither, Neither) -> True
+    (Both x y, Both x' y') -> f x x' && g y y'
+    (_, _) -> False
+
+instance (Ord a) => Ord1 (XNor a) where
+  -- TODO: Remove this once base-4.18 is the oldest supported verson, as it’s
+  --       the default impl.
+  liftCompare = liftCompare2 compare
+
+instance Ord2 XNor where
+  liftCompare2 f g = Tuple.curry $ \case
+    (Neither, Neither) -> EQ
+    (Neither, Both _ _) -> LT
+    (Both _ _, Neither) -> GT
+    (Both x y, Both x' y') -> f x x' <> g y y'
+
+instance (Show a) => Show1 (XNor a) where
+  -- TODO: Remove this once base-4.18 is the oldest supported verson, as it’s
+  --       the default impl.
+  liftShowsPrec = liftShowsPrec2 showsPrec showList
+
+instance Show2 XNor where
+  liftShowsPrec2 showsPrecX _showListX showsPrecY _showListY prec =
+    let appPrec = 10
+        nextPrec = appPrec + 1
+     in \case
+          Neither -> showString "Neither"
+          Both x y ->
+            showParen (nextPrec <= prec) $
+              showString "Both "
+                . showsPrecX nextPrec x
+                . showString " "
+                . showsPrecY nextPrec y
 
 instance Bifunctor XNor where
   bimap f g = \case
@@ -45,11 +95,58 @@ instance Bifunctor XNor where
 
 -- | Isomorphic to `(a, Maybe b)`, it’s also the pattern functor for non-empty
 --   lists.
-data AndMaybe a b = Only a | Indeed ~a b
-  deriving (Eq, Ord, Show, Functor, Foldable, Traversable)
+data AndMaybe a b = Only ~a | Indeed ~a b
+  deriving (Eq, Generic, Show, Foldable, Functor, Generic1, Traversable)
 
-deriveShow1 ''AndMaybe
-deriveShow2 ''AndMaybe
+instance (Eq a) => Eq1 (AndMaybe a) where
+  -- TODO: Remove this once base-4.18 is the oldest supported verson, as it’s
+  --       the default impl.
+  liftEq = liftEq2 (==)
+
+instance Eq2 AndMaybe where
+  liftEq2 f g = Tuple.curry $ \case
+    (Only x, Only x') -> f x x'
+    (Indeed x y, Indeed x' y') -> f x x' && g y y'
+    (_, _) -> False
+
+-- | This definition is different from the one that is derivable. For example,
+--   the derived instance would always have
+--   @`compare` (`Only` x) (`Indeed` x' y) `==` `LT`@, but this instance will
+--   return `GT` if @`compare` x x' `==` `GT`@.
+instance (Ord a, Ord b) => Ord (AndMaybe a b) where
+  compare = liftCompare compare
+
+instance (Ord a) => Ord1 (AndMaybe a) where
+  -- TODO: Remove this once base-4.18 is the oldest supported verson, as it’s
+  --       the default impl.
+  liftCompare = liftCompare2 compare
+
+instance Ord2 AndMaybe where
+  liftCompare2 f g = Tuple.curry $ \case
+    (Only x, Only x') -> f x x'
+    (Only x, Indeed x' _) -> f x x' <> LT
+    (Indeed x _, Only x') -> f x x' <> GT
+    (Indeed x y, Indeed x' y') -> f x x' <> g y y'
+
+instance (Show a) => Show1 (AndMaybe a) where
+  -- TODO: Remove this once base-4.18 is the oldest supported verson, as it’s
+  --       the default impl.
+  liftShowsPrec = liftShowsPrec2 showsPrec showList
+
+instance Show2 AndMaybe where
+  liftShowsPrec2 showsPrecX _showListX showsPrecY _showListY prec =
+    let appPrec = 10
+        nextPrec = appPrec + 1
+     in \case
+          Only x ->
+            showParen (nextPrec <= prec) $
+              showString "Only " . showsPrecX nextPrec x
+          Indeed x y ->
+            showParen (nextPrec <= prec) $
+              showString "Indeed "
+                . showsPrecX nextPrec x
+                . showString " "
+                . showsPrecY nextPrec y
 
 instance Bifunctor AndMaybe where
   bimap f g = \case
