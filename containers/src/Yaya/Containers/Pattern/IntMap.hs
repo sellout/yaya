5093334ce1a638e1a58c3b240d4262e0a0ffd50c
Greg Pfeil <greg@technomadic.org>
2024-02-26 00:41:29 -0700

    Add pattern functors for `containers` package

---
+++ containers/src/Yaya/Containers/Pattern/IntMap.hs
@@ -0,0 +1,110 @@
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module Yaya.Containers.Pattern.IntMap
+  ( IntMapF (..),
+  )
+where
+
+import "base" Control.Category (Category ((.)))
+import "base" Data.Bool (Bool (False, True), (&&))
+import "base" Data.Eq (Eq ((==)))
+import "base" Data.Foldable (Foldable)
+import "base" Data.Function (($))
+import "base" Data.Functor (Functor (fmap))
+import "base" Data.Functor.Classes
+  ( Eq1 (liftEq),
+    Eq2 (liftEq2),
+    Ord1 (liftCompare),
+    Ord2 (liftCompare2),
+    Show1 (liftShowsPrec),
+    Show2 (liftShowsPrec2),
+  )
+import "base" Data.Ord (Ord (compare, (<=)), Ordering (EQ, GT, LT))
+import "base" Data.Semigroup ((<>))
+import "base" Data.Traversable (Traversable)
+import qualified "base" Data.Tuple as Tuple
+import "base" GHC.Generics (Generic, Generic1)
+import "base" Text.Show (Show (showList, showsPrec), showParen, showString)
+import qualified "containers" Data.IntMap.Internal as IntMap
+import "yaya" Yaya.Fold
+  ( Projectable (project),
+    Recursive (cata),
+    Steppable (embed),
+  )
+import "base" Prelude (Num ((+)))
+
+data IntMapF a r
+  = NilF
+  | TipF IntMap.Key a
+  | BinF IntMap.Prefix IntMap.Mask r r
+  deriving (Eq, Ord, Generic, Show, Foldable, Functor, Generic1, Traversable)
+
+instance Projectable (->) (IntMap.IntMap a) (IntMapF a) where
+  project IntMap.Nil = NilF
+  project (IntMap.Tip key a) = TipF key a
+  project (IntMap.Bin prefix mask l r) = BinF prefix mask l r
+
+instance Recursive (->) (IntMap.IntMap a) (IntMapF a) where
+  cata φ = φ . fmap (cata φ) . project
+
+instance Steppable (->) (IntMap.IntMap a) (IntMapF a) where
+  embed NilF = IntMap.Nil
+  embed (TipF key a) = IntMap.Tip key a
+  embed (BinF prefix mask l r) = IntMap.Bin prefix mask l r
+
+instance (Eq a) => Eq1 (IntMapF a) where
+  -- TODO: Remove this once base-4.18 is the oldest supported verson, as it’s
+  --       the default impl.
+  liftEq = liftEq2 (==)
+
+instance Eq2 IntMapF where
+  liftEq2 f g = Tuple.curry $ \case
+    (NilF, NilF) -> True
+    (TipF key a, TipF key' a') -> key == key' && f a a'
+    (BinF prefix mask l r, BinF prefix' mask' l' r') ->
+      prefix == prefix' && mask == mask' && g l l' && g r r'
+    (_, _) -> False
+
+instance (Ord a) => Ord1 (IntMapF a) where
+  -- TODO: Remove this once base-4.18 is the oldest supported verson, as it’s
+  --       the default impl.
+  liftCompare = liftCompare2 compare
+
+instance Ord2 IntMapF where
+  liftCompare2 f g = Tuple.curry $ \case
+    (NilF, NilF) -> EQ
+    (NilF, _) -> LT
+    (TipF {}, NilF) -> GT
+    (TipF key a, TipF key' a') -> compare key key' <> f a a'
+    (TipF {}, BinF {}) -> LT
+    (BinF prefix mask l r, BinF prefix' mask' l' r') ->
+      compare prefix prefix' <> compare mask mask' <> g l l' <> g r r'
+    (BinF {}, _) -> GT
+
+instance (Show a) => Show1 (IntMapF a) where
+  -- TODO: Remove this once base-4.18 is the oldest supported verson, as it’s
+  --       the default impl.
+  liftShowsPrec = liftShowsPrec2 showsPrec showList
+
+instance Show2 IntMapF where
+  liftShowsPrec2 showsPrecA _showListA showsPrecR _showListR prec =
+    let appPrec = 10
+        nextPrec = appPrec + 1
+     in \case
+          NilF -> showString "NilF"
+          TipF key a ->
+            showParen (nextPrec <= prec) $
+              showString "BipF "
+                . showsPrec nextPrec key
+                . showString " "
+                . showsPrecA nextPrec a
+          BinF prefix mask l r ->
+            showParen (nextPrec <= prec) $
+              showString "BinF "
+                . showsPrec nextPrec prefix
+                . showString " "
+                . showsPrec nextPrec mask
+                . showString " "
+                . showsPrecR nextPrec l
+                . showString " "
+                . showsPrecR nextPrec r
---
+++ containers/src/Yaya/Containers/Pattern/IntSet.hs
@@ -0,0 +1,93 @@
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module Yaya.Containers.Pattern.IntSet
+  ( IntSetF (..),
+  )
+where
+
+import "base" Control.Category (Category ((.)))
+import "base" Data.Bool (Bool (False, True), (&&))
+import "base" Data.Eq (Eq ((==)))
+import "base" Data.Foldable (Foldable)
+import "base" Data.Function (($))
+import "base" Data.Functor (Functor (fmap))
+import "base" Data.Functor.Classes
+  ( Eq1 (liftEq),
+    Ord1 (liftCompare),
+    Show1 (liftShowsPrec),
+  )
+import "base" Data.Ord (Ord (compare, (<=)), Ordering (EQ, GT, LT))
+import "base" Data.Semigroup ((<>))
+import "base" Data.Traversable (Traversable)
+import qualified "base" Data.Tuple as Tuple
+import "base" GHC.Generics (Generic, Generic1)
+import "base" Text.Show (Show (showsPrec), showParen, showString)
+import qualified "containers" Data.IntSet.Internal as IntSet
+import "yaya" Yaya.Fold
+  ( Projectable (project),
+    Recursive (cata),
+    Steppable (embed),
+  )
+import "base" Prelude (Num ((+)))
+
+data IntSetF r
+  = NilF
+  | TipF IntSet.Prefix IntSet.BitMap
+  | BinF IntSet.Prefix IntSet.Mask r r
+  deriving (Eq, Ord, Generic, Show, Foldable, Functor, Generic1, Traversable)
+
+instance Projectable (->) IntSet.IntSet IntSetF where
+  project IntSet.Nil = NilF
+  project (IntSet.Tip prefix bm) = TipF prefix bm
+  project (IntSet.Bin prefix mask l r) = BinF prefix mask l r
+
+instance Recursive (->) IntSet.IntSet IntSetF where
+  cata φ = φ . fmap (cata φ) . project
+
+instance Steppable (->) IntSet.IntSet IntSetF where
+  embed NilF = IntSet.Nil
+  embed (TipF prefix bm) = IntSet.Tip prefix bm
+  embed (BinF prefix mask l r) = IntSet.Bin prefix mask l r
+
+instance Eq1 IntSetF where
+  liftEq f = Tuple.curry $ \case
+    (NilF, NilF) -> True
+    (TipF prefix bm, TipF prefix' bm') -> prefix == prefix' && bm == bm'
+    (BinF prefix mask l r, BinF prefix' mask' l' r') ->
+      prefix == prefix' && mask == mask' && f l l' && f r r'
+    (_, _) -> False
+
+instance Ord1 IntSetF where
+  liftCompare f = Tuple.curry $ \case
+    (NilF, NilF) -> EQ
+    (NilF, _) -> LT
+    (TipF {}, NilF) -> GT
+    (TipF prefix bm, TipF prefix' bm') ->
+      compare prefix prefix' <> compare bm bm'
+    (TipF {}, BinF {}) -> LT
+    (BinF prefix mask l r, BinF prefix' mask' l' r') ->
+      compare prefix prefix' <> compare mask mask' <> f l l' <> f r r'
+    (BinF {}, _) -> GT
+
+instance Show1 IntSetF where
+  liftShowsPrec showsPrecR _showListR prec =
+    let appPrec = 10
+        nextPrec = appPrec + 1
+     in \case
+          NilF -> showString "NilF"
+          TipF prefix bm ->
+            showParen (nextPrec <= prec) $
+              showString "TipF "
+                . showsPrec nextPrec prefix
+                . showString " "
+                . showsPrec nextPrec bm
+          BinF prefix mask l r ->
+            showParen (nextPrec <= prec) $
+              showString "BinF "
+                . showsPrec nextPrec prefix
+                . showString " "
+                . showsPrec nextPrec mask
+                . showString " "
+                . showsPrecR nextPrec l
+                . showString " "
+                . showsPrecR nextPrec r
