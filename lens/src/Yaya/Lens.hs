d68463b019c36874fbdeaab6386fee9f3fd7fe1d
Greg Pfeil <greg@technomadic.org>
2026-01-27 16:36:30 -0700

    Improve yaya-lens
    Do some renaming, and generalize the types to ones that can actually be used.
    Also add a `Lens` that’s dual to the existing `Prism`.

--- lens/src/Yaya/Lens.hs
+++ lens/src/Yaya/Lens.hs
@@ -1,22 +1,40 @@
 {-# LANGUAGE Safe #-}
 
 module Yaya.Lens
-  ( AlgebraPrism,
+  ( AlgebraLens,
+    AlgebraLens',
     BialgebraIso,
+    BialgebraIso',
     CoalgebraPrism,
+    CoalgebraPrism',
     birecursiveIso,
-    recursivePrism,
+    folded,
     steppableIso,
+    unfolded,
   )
 where
 
+import "base" Control.Category ((.))
 import "base" Control.Monad ((<=<))
 import "base" Data.Bifunctor (first)
-import "base" Data.Function (const, ($))
+import "base" Data.Function (const, flip, ($))
+import "base" Data.Functor ((<$>))
 import "base" Data.Traversable (Traversable, sequenceA)
-import "lens" Control.Lens (Iso', Prism', iso, matching, prism, review, view)
+import "lens" Control.Lens
+  ( Iso,
+    Lens,
+    Prism,
+    iso,
+    lens,
+    matching,
+    prism,
+    review,
+    set,
+    view,
+  )
 import "yaya" Yaya.Fold
   ( Corecursive,
+    Projectable,
     Recursive,
     Steppable,
     ana,
@@ -25,26 +43,56 @@ import "yaya" Yaya.Fold
     project,
   )
 
-type BialgebraIso f a = Iso' (f a) a
+-- | An isomorphism between an @`Algebra` (->) f a@ and a @`Coalgebra` (->) g b@.
+type BialgebraIso f g a b = Iso (f a) (g b) a b
 
-type AlgebraPrism f a = Prism' (f a) a
+type BialgebraIso' f a = BialgebraIso f f a a
 
-type CoalgebraPrism f a = Prism' a (f a)
+type AlgebraLens f g a b = Lens (f a) (g b) a b
 
-steppableIso :: (Steppable (->) t f) => BialgebraIso f t
+type AlgebraLens' f a = AlgebraLens f f a a
+
+-- | A prism between an @`AlgebraM` (->) (`Either` (f a)) f a@ and a
+--   @`Coalgebra` g b@.
+type CoalgebraPrism f g a b = Prism (f a) (g b) a b
+
+type CoalgebraPrism' f a = CoalgebraPrism f f a a
+
+-- | The isomorphism between `embed` and `project`.
+steppableIso ::
+  (Steppable (->) t f, Projectable (->) u g) => BialgebraIso f g t u
 steppableIso = iso embed project
 
+-- | The isomorphism between `cata` and `ana` as a function from an isomorphism
+--   between (co)algebras to an isomorphism between the (co)recursive structure
+--   and its folded value.
 birecursiveIso ::
-  (Recursive (->) t f, Corecursive (->) t f) =>
-  BialgebraIso f a ->
-  Iso' t a
-birecursiveIso alg = iso (cata (view alg)) (ana (review alg))
+  (Recursive (->) s f, Corecursive (->) t f) =>
+  BialgebraIso' f a ->
+  Iso s t a a
+birecursiveIso φ = iso (cata (view φ)) (ana (review φ))
 
-recursivePrism ::
-  (Recursive (->) t f, Corecursive (->) t f, Traversable f) =>
-  AlgebraPrism f a ->
-  Prism' t a
-recursivePrism alg =
+folded ::
+  ( Recursive (->) s f,
+    Projectable (->) s f,
+    Corecursive (->) t f,
+    Traversable f
+  ) =>
+  AlgebraLens' f a ->
+  Lens s t a a
+folded φ =
+  lens (cata (view φ)) (\s -> ana . flip (set φ) $ cata (view φ) <$> project s)
+
+-- | The prism between an unfold and a monadic fold.
+unfolded ::
+  ( Recursive (->) s f,
+    Projectable (->) s f,
+    Corecursive (->) t f,
+    Traversable f
+  ) =>
+  CoalgebraPrism' f a ->
+  Prism s t a a
+unfolded ψ =
   prism
-    (ana (review alg))
-    (\t -> first (const t) $ cata (matching alg <=< sequenceA) t)
+    (ana $ review ψ)
+    (\s -> first (const $ ana project s) $ cata (matching ψ <=< sequenceA) s)
