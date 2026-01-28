91fef97d2cac9bb80f2295931d32727b15409d79
Greg Pfeil <greg@technomadic.org>
2026-01-27 18:08:41 -0700

    Remove the trailing `F` from container functors

--- containers/src/Yaya/Containers/Pattern/IntSet.hs
+++ containers/src/Yaya/Containers/Pattern/IntSet.hs
@@ -3,7 +3,7 @@
 {-# OPTIONS_GHC -Wno-orphans #-}
 
 module Yaya.Containers.Pattern.IntSet
-  ( IntSetF (BinF, NilF, TipF),
+  ( IntSet (Bin, Nil, Tip),
   )
 where
 
@@ -49,117 +49,116 @@ import "base" Prelude ((+))
 import qualified "containers" Data.IntSet.Internal.IntTreeCommons as IntSet
   ( Prefix (Prefix),
   )
-
-data IntSetF r
-  = NilF
-  | TipF Int IntSet.BitMap
-  | BinF IntSet.Prefix r r
-  deriving stock
-    ( Eq,
-      Generic,
-      Foldable,
-      Functor,
-      Generic1,
-      Traversable
-    )
-#else
-data IntSetF r
-  = NilF
-  | TipF Int IntSet.BitMap
-  | BinF IntSet.Prefix IntSet.Mask r r
-  deriving stock
-    ( Eq,
-      Generic,
-      Foldable,
-      Functor,
-      Generic1,
-      Traversable
-    )
 #endif
 
-instance Projectable (->) IntSet.IntSet IntSetF where
-  project = \case
-    IntSet.Nil -> NilF
-    IntSet.Tip prefix bm -> TipF prefix bm
+data IntSet r
+  = Nil
+  | Tip Int IntSet.BitMap
 #if MIN_VERSION_containers(0, 8, 0)
-    IntSet.Bin prefix l r -> BinF prefix l r
+  | Bin IntSet.Prefix r r
+  deriving stock
+    ( Eq,
+      Generic,
+      Foldable,
+      Functor,
+      Generic1,
+      Traversable
+    )
 #else
-    IntSet.Bin prefix mask l r -> BinF prefix mask l r
+  | Bin IntSet.Prefix IntSet.Mask r r
+  deriving stock
+    ( Eq,
+      Generic,
+      Foldable,
+      Functor,
+      Generic1,
+      Traversable
+    )
 #endif
 
-instance Recursive (->) IntSet.IntSet IntSetF where
+instance Projectable (->) IntSet.IntSet IntSet where
+  project = \case
+    IntSet.Nil -> Nil
+    IntSet.Tip prefix bm -> Tip prefix bm
+#if MIN_VERSION_containers(0, 8, 0)
+    IntSet.Bin prefix l r -> Bin prefix l r
+#else
+    IntSet.Bin prefix mask l r -> Bin prefix mask l r
+#endif
+
+instance Recursive (->) IntSet.IntSet IntSet where
   cata = Unsafe.unsafeCata
 
-instance Steppable (->) IntSet.IntSet IntSetF where
+instance Steppable (->) IntSet.IntSet IntSet where
   embed = \case
-    NilF -> IntSet.Nil
-    TipF prefix bm -> IntSet.Tip prefix bm
+    Nil -> IntSet.Nil
+    Tip prefix bm -> IntSet.Tip prefix bm
 #if MIN_VERSION_containers(0, 8, 0)
-    BinF prefix l r -> IntSet.Bin prefix l r
+    Bin prefix l r -> IntSet.Bin prefix l r
 #else
-    BinF prefix mask l r -> IntSet.Bin prefix mask l r
+    Bin prefix mask l r -> IntSet.Bin prefix mask l r
 #endif
 
-instance Eq1 IntSetF where
+instance Eq1 IntSet where
   liftEq f = Tuple.curry $ \case
-    (NilF, NilF) -> True
-    (NilF, _) -> False
-    (_, NilF) -> False
-    (TipF prefix bm, TipF prefix' bm') -> prefix == prefix' && bm == bm'
-    (TipF {}, _) -> False
-    (_, TipF {}) -> False
+    (Nil, Nil) -> True
+    (Nil, _) -> False
+    (_, Nil) -> False
+    (Tip prefix bm, Tip prefix' bm') -> prefix == prefix' && bm == bm'
+    (Tip {}, _) -> False
+    (_, Tip {}) -> False
 #if MIN_VERSION_containers(0, 8, 0)
-    (BinF prefix l r, BinF prefix' l' r') ->
+    (Bin prefix l r, Bin prefix' l' r') ->
       prefix == prefix' && f l l' && f r r'
 #else
-    (BinF prefix mask l r, BinF prefix' mask' l' r') ->
+    (Bin prefix mask l r, Bin prefix' mask' l' r') ->
       prefix == prefix' && mask == mask' && f l l' && f r r'
 #endif
 
-instance (Ord r) => Ord (IntSetF r) where
+instance (Ord r) => Ord (IntSet r) where
   compare = liftCompare compare
 
-instance Ord1 IntSetF where
+instance Ord1 IntSet where
   liftCompare f = Tuple.curry $ \case
-    (NilF, NilF) -> EQ
-    (NilF, _) -> LT
-    (TipF {}, NilF) -> GT
-    (TipF prefix bm, TipF prefix' bm') ->
+    (Nil, Nil) -> EQ
+    (Nil, _) -> LT
+    (Tip {}, Nil) -> GT
+    (Tip prefix bm, Tip prefix' bm') ->
       compare prefix prefix' <> compare bm bm'
-    (TipF {}, BinF {}) -> LT
-    (BinF {}, NilF) -> GT
-    (BinF {}, TipF {}) -> GT
+    (Tip {}, Bin {}) -> LT
+    (Bin {}, Nil) -> GT
+    (Bin {}, Tip {}) -> GT
 #if MIN_VERSION_containers(0, 8, 0)
-    (BinF (IntSet.Prefix prefix) l r, BinF (IntSet.Prefix prefix') l' r') ->
+    (Bin (IntSet.Prefix prefix) l r, Bin (IntSet.Prefix prefix') l' r') ->
       compare prefix prefix' <> f l l' <> f r r'
 #else
-    (BinF prefix mask l r, BinF prefix' mask' l' r') ->
+    (Bin prefix mask l r, Bin prefix' mask' l' r') ->
       compare prefix prefix' <> compare mask mask' <> f l l' <> f r r'
 #endif
 
 -- | @since 0.1.2.0
-instance (Read r) => Read (IntSetF r) where
+instance (Read r) => Read (IntSet r) where
   readPrec = liftReadPrec readPrec readListPrec
 
 -- | @since 0.1.2.0
-instance Read1 IntSetF where
+instance Read1 IntSet where
   liftReadPrec readPrecR _ =
     let appPrec = 10
      in parens . prec appPrec $
-          NilF
-            <$ expectP (Lex.Ident "NilF")
-            <|> expectP (Lex.Ident "TipF")
-              *> (TipF <$> step readPrec <*> step readPrec)
+          Nil
+            <$ expectP (Lex.Ident "Nil")
+            <|> expectP (Lex.Ident "Tip")
+              *> (Tip <$> step readPrec <*> step readPrec)
 #if MIN_VERSION_containers(0, 8, 0)
-            <|> expectP (Lex.Ident "BinF")
-              *> ( BinF . IntSet.Prefix
+            <|> expectP (Lex.Ident "Bin")
+              *> ( Bin . IntSet.Prefix
                      <$> step readPrec
                      <*> step readPrecR
                      <*> step readPrecR
                  )
 #else
-            <|> expectP (Lex.Ident "BinF")
-              *> ( BinF
+            <|> expectP (Lex.Ident "Bin")
+              *> ( Bin
                      <$> step readPrec
                      <*> step readPrec
                      <*> step readPrecR
@@ -167,34 +166,34 @@ instance Read1 IntSetF where
                  )
 #endif
 
-instance (Show r) => Show (IntSetF r) where
+instance (Show r) => Show (IntSet r) where
   showsPrec = liftShowsPrec showsPrec showList
 
-instance Show1 IntSetF where
+instance Show1 IntSet where
   liftShowsPrec showsPrecR _ p =
     let appPrec = 10
         nextPrec = appPrec + 1
      in \case
-          NilF -> showString "NilF"
-          TipF prefix bm ->
+          Nil -> showString "Nil"
+          Tip prefix bm ->
             showParen (nextPrec <= p) $
-              showString "TipF "
+              showString "Tip "
                 . showsPrec nextPrec prefix
                 . showString " "
                 . showsPrec nextPrec bm
 #if MIN_VERSION_containers(0, 8, 0)
-          BinF (IntSet.Prefix prefix) l r ->
+          Bin (IntSet.Prefix prefix) l r ->
             showParen (nextPrec <= p) $
-              showString "BinF "
+              showString "Bin "
                 . showsPrec nextPrec prefix
                 . showString " "
                 . showsPrecR nextPrec l
                 . showString " "
                 . showsPrecR nextPrec r
 #else
-          BinF prefix mask l r ->
+          Bin prefix mask l r ->
             showParen (nextPrec <= p) $
-              showString "BinF "
+              showString "Bin "
                 . showsPrec nextPrec prefix
                 . showString " "
                 . showsPrec nextPrec mask
